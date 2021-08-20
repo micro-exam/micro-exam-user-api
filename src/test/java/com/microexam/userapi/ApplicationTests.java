a58eb53ce8cec3ceb36ab2c28713ce5c348fb493
Avani Patel <avanirpatel6@gmail.com>
2021-08-19 23:27:24 -0400

    initial setup

---
+++ src/test/java/com/microexam/userapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microexam.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
